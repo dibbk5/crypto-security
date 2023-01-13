a530abeefa591a361944cdd8ae13fc5f0d40979c
Koy Dibb <dibbk5@gmail.com>
2023-01-12 19:23:58 -0700

    Final Commit

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,21 +1,40 @@
-const users = []
+let users = [];
+const bcryptjs = require("bcryptjs");
 
 module.exports = {
-    login: (req, res) => {
-      console.log('Logging In User')
-      console.log(req.body)
-      const { username, password } = req.body
-      for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+  login: (req, res) => {
+    // console.log("Logging In User");
+    // console.log(req.body);
+    const { username, password } = req.body;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === username) {
+        const checkUser = bcryptjs.compareSync(password, users[i].saltPassword);
+        if (checkUser) {
+          res.status(200).send(users[i]);
         }
       }
-      res.status(400).send("User not found.")
-    },
-    register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
     }
-}
+    res.status(400).send("User not found.");
+  },
+  register: (req, res) => {
+    let { username, email, firstName, lastName, password, password2 } =
+      req.body;
+
+    if (!username || !email || !firstName || !lastName || !password) {
+      return res.status(400).send("Missing Information");
+    }
+
+    const salt = bcryptjs.genSaltSync(4);
+    const saltPassword = bcryptjs.hashSync(password, salt);
+
+    const newUser = {
+      username,
+      email,
+      firstName,
+      lastName,
+      saltPassword,
+    };
+    users.push(newUser);
+    res.status(200).send(req.body);
+  },
+};
